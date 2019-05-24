c0f91987fa3c5884911af5781b7fb0a0b5f0d2f9
pl61 <leisureemail1@gmail.com>
2019-05-23 23:30:39 -0700

    added jquery

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './styles/style.less';
+import './scripts.js';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
