1cf1268a41dfa864c1b877e8d1d38fa7c91c5cdd
pl61 <leisureemail1@gmail.com>
2019-05-26 21:04:03 -0700

    changed component structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar.jsx';
-import Content from './Content.jsx';
+import About from './pages/About.jsx';
+import Skills from './pages/Skills.jsx';
+import Apps from './pages/Apps.jsx';
 
 const App = () => {
   const [activeItem, setActiveItem] = useState('about-item');
 
+  const selectContent = (selected) => {
+    switch(selected) {
+      case 'about-item':
+        return <About/>;
+      case 'skills-item':
+        return <Skills/>;
+      case 'apps-item':
+        return <Apps/>;
+      default:
+        return 'This should never show up';
+    }
+  }
+
   return (
     <div>
       <Navbar activeItem={activeItem} setActiveItem={setActiveItem}/>
-      <Content activeItem={activeItem}/>
+      {selectContent(activeItem)}
     </div>
   );
 };
---
+++ src/components/pages/Apps.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Apps = () => {
+  return (
+    <div className="content">
+      apps-item
+    </div>
+  );
+};
+
+export default Apps;
